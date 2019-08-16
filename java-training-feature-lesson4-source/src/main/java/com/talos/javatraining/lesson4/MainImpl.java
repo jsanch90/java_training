b33f8357e4476d1c19e8ba18cece837d34cd5636
joshuasanchez <jsanch90@github.com>
2019-08-16 16:32:41 -0500

    Lesson 4 completed

--- java-training-feature-lesson4-source/src/main/java/com/talos/javatraining/lesson4/MainImpl.java
+++ java-training-feature-lesson4-source/src/main/java/com/talos/javatraining/lesson4/MainImpl.java
@@ -11,6 +11,8 @@ import org.apache.commons.lang3.StringUtils;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
+import java.util.Optional;
+import java.util.StringJoiner;
 import java.util.function.Predicate;
 
 
@@ -18,101 +20,172 @@ public class MainImpl implements Main
 {
 
 	@Override
+//	public String getLine1(AddressModel addressModel)
+//	{
+//		String result = StringUtils.EMPTY;
+//		if (addressModel != null)
+//		{
+//			if (StringUtils.isNotBlank(addressModel.getLine1()))
+//			{
+//				result = addressModel.getLine1();
+//			}
+//		}
+//		return result;
+//	}
+
+
+
 	public String getLine1(AddressModel addressModel)
 	{
-		String result = StringUtils.EMPTY;
-		if (addressModel != null)
-		{
-			if (StringUtils.isNotBlank(addressModel.getLine1()))
-			{
-				result = addressModel.getLine1();
-			}
-		}
-		return result;
+		Optional<AddressModel> optAddress = Optional.ofNullable(addressModel);
+		return optAddress.map(AddressModel::getLine1)
+				.filter(StringUtils::isNotBlank)
+				.orElse(StringUtils.EMPTY);
+
 	}
 
+
 	@Override
+//	public String getFullName(AddressModel addressModel)
+//{
+//	StringBuilder stringBuilder = new StringBuilder();
+//	if (addressModel != null)
+//	{
+//		if (StringUtils.isNotBlank(addressModel.getFirstName()))
+//		{
+//			stringBuilder.append(addressModel.getFirstName());
+//		}
+//		if (StringUtils.isNotBlank(addressModel.getLastName()))
+//		{
+//			if (stringBuilder.length() != 0)
+//			{
+//				stringBuilder.append(StringUtils.SPACE);
+//			}
+//			stringBuilder.append(addressModel.getLastName());
+//		}
+//	}
+//	return stringBuilder.toString();
+//}
+
 	public String getFullName(AddressModel addressModel)
 	{
-		StringBuilder stringBuilder = new StringBuilder();
-		if (addressModel != null)
-		{
-			if (StringUtils.isNotBlank(addressModel.getFirstName()))
-			{
-				stringBuilder.append(addressModel.getFirstName());
-			}
-			if (StringUtils.isNotBlank(addressModel.getLastName()))
-			{
-				if (stringBuilder.length() != 0)
-				{
-					stringBuilder.append(StringUtils.SPACE);
-				}
-				stringBuilder.append(addressModel.getLastName());
-			}
-		}
-		return stringBuilder.toString();
+		StringJoiner stringJoiner = new StringJoiner(" ").setEmptyValue(StringUtils.EMPTY);
+
+
+		Optional.ofNullable(addressModel)
+				.map(AddressModel::getFirstName)
+				.filter(StringUtils::isNotBlank)
+				.ifPresent(x -> stringJoiner.add(x));
+
+		Optional.ofNullable(addressModel)
+				.map(AddressModel::getLastName)
+				.filter(StringUtils::isNotBlank)
+				.ifPresent(x -> stringJoiner.add(x));
+
+		return stringJoiner.toString();
 	}
 
 	@Override
+//	public AddressModel getBillingAddress(UserModel userModel)
+//	{
+//		AddressModel result = null;
+//		if (userModel != null)
+//		{
+//			if (CollectionUtils.isNotEmpty(userModel.getAddresses()))
+//			{
+//				result = getAddress(userModel.getAddresses(), a -> BooleanUtils.isTrue(a.getBillingAddress()));
+//			}
+//		}
+//		return result;
+//	}
+
 	public AddressModel getBillingAddress(UserModel userModel)
 	{
-		AddressModel result = null;
-		if (userModel != null)
-		{
-			if (CollectionUtils.isNotEmpty(userModel.getAddresses()))
-			{
-				result = getAddress(userModel.getAddresses(), a -> BooleanUtils.isTrue(a.getBillingAddress()));
-			}
-		}
-		return result;
+         return Optional.ofNullable(userModel)
+				 .map(UserModel::getAddresses)
+				 .filter(CollectionUtils::isNotEmpty)
+				 .map(x -> getAddress(x, (y) -> BooleanUtils.isTrue(y.getBillingAddress()))).orElse(null);
 	}
 
 	@Override
+//	public String getLastLoginFormatted(UserModel userModel)
+//	{
+//		DateFormat format = new SimpleDateFormat("MM/dd/yyyy");
+//		String result = "the user has not been logged yet";
+//		if (userModel != null && userModel.getLastLogin() != null)
+//		{
+//			result = format.format(userModel.getLastLogin());
+//		}
+//		return result;
+//	}
+
 	public String getLastLoginFormatted(UserModel userModel)
 	{
 		DateFormat format = new SimpleDateFormat("MM/dd/yyyy");
-		String result = "the user has not been logged yet";
-		if (userModel != null && userModel.getLastLogin() != null)
-		{
-			result = format.format(userModel.getLastLogin());
-		}
-		return result;
+		return Optional.ofNullable(userModel)
+				.filter(x -> x.getLastLogin() != null)
+				.map(UserModel::getLastLogin)
+				.map(format::format)
+				.orElse("the user has not been logged yet");
 	}
 
 	@Override
+//	public String getContactCountry(UserModel userModel)
+//	{
+//		String contactAddressIsoCode = null;
+//		if (userModel != null)
+//		{
+//			if (CollectionUtils.isNotEmpty(userModel.getAddresses()))
+//			{
+//				AddressModel contactAddress = getAddress(userModel.getAddresses(), a -> BooleanUtils.isTrue(a.getContactAddress()));
+//				if (contactAddress != null && contactAddress.getCountry() != null)
+//				{
+//					contactAddressIsoCode = contactAddress.getCountry().getIsocode();
+//				}
+//			}
+//		}
+//		if (contactAddressIsoCode == null)
+//		{
+//			contactAddressIsoCode = inferCountry();
+//		}
+//		return contactAddressIsoCode;
+//	}
+
 	public String getContactCountry(UserModel userModel)
 	{
-		String contactAddressIsoCode = null;
-		if (userModel != null)
-		{
-			if (CollectionUtils.isNotEmpty(userModel.getAddresses()))
-			{
-				AddressModel contactAddress = getAddress(userModel.getAddresses(), a -> BooleanUtils.isTrue(a.getContactAddress()));
-				if (contactAddress != null && contactAddress.getCountry() != null)
-				{
-					contactAddressIsoCode = contactAddress.getCountry().getIsocode();
-				}
-			}
-		}
-		if (contactAddressIsoCode == null)
-		{
-			contactAddressIsoCode = inferCountry();
-		}
-		return contactAddressIsoCode;
+
+		return Optional.ofNullable(userModel)
+				.map(UserModel::getAddresses)
+				.filter(CollectionUtils::isNotEmpty)
+				.map(x -> getAddress(x, (y) -> BooleanUtils.isTrue(y.getContactAddress())))
+				.filter(x -> x != null)
+				.filter(x -> x.getCountry() != null)
+				.map(x -> x.getCountry().getIsocode())
+				.orElseGet(this::inferCountry);
+
 	}
 
 	@Override
+//	public AddressModel getShippingAddress(UserModel userModel) throws AddressNotFoundException
+//	{
+//		AddressModel addressModel = null;
+//		if (CollectionUtils.isNotEmpty(userModel.getAddresses()))
+//		{
+//			addressModel = getAddress(userModel.getAddresses(), a -> BooleanUtils.isTrue(a.getShippingAddress()));
+//		}
+//		if(addressModel == null){
+//			throw new AddressNotFoundException();
+//		}
+//		return addressModel;
+//	}
+
 	public AddressModel getShippingAddress(UserModel userModel) throws AddressNotFoundException
 	{
-		AddressModel addressModel = null;
-		if (CollectionUtils.isNotEmpty(userModel.getAddresses()))
-		{
-			addressModel = getAddress(userModel.getAddresses(), a -> BooleanUtils.isTrue(a.getShippingAddress()));
-		}
-		if(addressModel == null){
-			throw new AddressNotFoundException();
-		}
-		return addressModel;
+		return Optional.ofNullable(userModel)
+				.map(UserModel::getAddresses)
+				.filter(CollectionUtils::isNotEmpty)
+				.map(x -> getAddress(x, (y) -> BooleanUtils.isTrue(y.getShippingAddress())))
+				.orElseThrow(AddressNotFoundException::new);
 	}
 
 	// ----------------------------------
