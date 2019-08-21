d729d9abad910d5bb9699ae2c2a0ba5dd8a9af92
joshuasanchez <jsanch90@github.com>
2019-08-21 17:08:21 -0500

    Lesson 7 completed

--- java-training-feature-lesson7-source/src/main/java/com/talos/javatraining/lesson7/MainImpl.java
+++ java-training-feature-lesson7-source/src/main/java/com/talos/javatraining/lesson7/MainImpl.java
@@ -4,48 +4,40 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Predicate;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
+import java.util.stream.BaseStream;
+import java.util.Collection;
+
 import org.apache.commons.lang3.mutable.MutableObject;
 
 
-public class MainImpl implements Main
-{
-	@Override
-	public BigDecimal sum(Stream<String> stream)
-	{
-		MutableObject<BigDecimal> mutableAccumulator = new MutableObject<>(BigDecimal.ZERO);
+public class MainImpl implements Main {
 
-		stream.map(BigDecimal::new).forEach(v -> mutableAccumulator.setValue(mutableAccumulator.getValue().add(v)));
+    @Override
+    public BigDecimal sum(Stream<String> stream) {
+        return stream.parallel()
+                .map(BigDecimal::new)
+                .reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
+    }
 
-		return mutableAccumulator.getValue();
-	}
+    @Override
+    public BigDecimal sumIf(Stream<String> stream, Predicate<BigDecimal> predicate) {
+        return stream.parallel()
+                .map(BigDecimal::new)
+                .filter(predicate)
+                .reduce(BigDecimal::add).orElse(BigDecimal.ZERO);
+    }
 
-	@Override
-	public BigDecimal sumIf(Stream<String> stream, Predicate<BigDecimal> predicate)
-	{
-		MutableObject<BigDecimal> mutableAccumulator = new MutableObject<>(BigDecimal.ZERO);
-		stream.map(BigDecimal::new).filter(predicate)
-				.forEach(v -> mutableAccumulator.setValue(mutableAccumulator.getValue().add(v)));
+    @Override
+    public Map<Long, BigDecimal> sumsByRange(Stream<String> stream) {
 
-		return mutableAccumulator.getValue();
-	}
+        return stream.parallel()
+                .map(BigDecimal::new)
+                .collect(Collectors.toMap(this::getRange, num -> num, BigDecimal::add));
+    }
 
-	@Override
-	public Map<Long, BigDecimal> sumsByRange(Stream<String> stream)
-	{
-		Map<Long, BigDecimal> result = new HashMap<>();
-		stream.map(BigDecimal::new).forEach(e -> result.compute(getRange(e), (k, v) -> {
-			if (v == null)
-			{
-				v = BigDecimal.ZERO;
-			}
-			return v.add(e);
-		}));
-		return result;
-	}
-
-	private Long getRange(BigDecimal value)
-	{
-		return value.movePointLeft(3).longValue();
-	}
+    private Long getRange(BigDecimal value) {
+        return value.movePointLeft(3).longValue();
+    }
 }
